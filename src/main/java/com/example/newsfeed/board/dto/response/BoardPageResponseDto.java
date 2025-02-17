517c00b172f1851ae5d47066a6e211b1ca5b607c
karyosome1 <87289805+karyosome1@users.noreply.github.com>
2025-02-17 13:56:32 +0900

    feat(board): 게시물 CRUD 기능 구현
    *  feat(board): 요구사항 작성
    
    * feat(board): 패키지 생성
    
    * feat(board): 게시물 CRUD 기능 구현
    
    * refactor(board): const를 common으로 이동, @RequestMapping(/api/v1) 추가, 메서드 이름 수정, dto 패키지 request, response로 나누기
    
    ---------
    
    Co-authored-by: JeonTak <76037656+JEONTAK@users.noreply.github.com>

---
+++ src/main/java/com/example/newsfeed/board/dto/response/BoardPageResponseDto.java
@@ -0,0 +1,27 @@
+package com.example.newsfeed.board.dto.response;
+
+import lombok.Getter;
+import java.time.LocalDateTime;
+
+
+@Getter
+public class BoardPageResponseDto {
+    private final Long id;
+    private final String title;
+    private final String contents;
+    private final int commentCount;
+    private final LocalDateTime createdAt;
+    private final LocalDateTime modifiedAt;
+    private final String userName;
+
+
+    public BoardPageResponseDto(Long id, String title, String contents, int commentCount, LocalDateTime createdAt, LocalDateTime modifiedAt, String userName) {
+        this.id = id;
+        this.title = title;
+        this.contents = contents;
+        this.commentCount = commentCount;
+        this.createdAt = createdAt;
+        this.modifiedAt = modifiedAt;
+        this.userName = userName;
+    }
+}
