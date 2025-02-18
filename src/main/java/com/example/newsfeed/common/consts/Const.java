10812797e1dd5614c0a7d61305aa7779891baff7
karyosome1 <ammonite0880@naver.com>
2025-02-18 15:20:27 +0900

    feat(board) : Validation 적용

--- src/main/java/com/example/newsfeed/common/consts/Const.java
+++ src/main/java/com/example/newsfeed/common/consts/Const.java
@@ -3,4 +3,16 @@ package com.example.newsfeed.common.consts;
 public abstract class Const {
 
     public static final String LOGIN_USER = "LOGIN_USER";
+
+    public static final String TITLE_NOT_NULL = "제목은 필수 입력값입니다.";
+    public static final String TITLE_SIZE = "제목은 최대 20글자 입니다.";
+
+    public static final String CONTENTS_NOT_NULL = "내용은 필수 입력값입니다.";
+    public static final String CONTENTS_SIZE = "내용은 최대 200자 입니다.";
+
+    public static final String IMAGES_NOT_NULL = "이미지는 필수 입력값입니다.";
+    public static final String IMAGES_PATTERN = ".*\\.(png|jpg|jpeg|gif)$";
+    public static final String IMAGES_PATTERN__REQUIREMENT = "허용된 확장자는 png, jpg, jpeg, gif 입니다.";
+
+
 }
--- src/main/java/com/example/newsfeed/image/entity/BoardImage.java
+++ src/main/java/com/example/newsfeed/image/entity/BoardImage.java
@@ -12,9 +12,13 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.web.multipart.MultipartFile;
+
+import static com.example.newsfeed.common.consts.Const.IMAGES_PATTERN;
+import static com.example.newsfeed.common.consts.Const.IMAGES_PATTERN__REQUIREMENT;
 
 @Getter
 @Entity
@@ -26,6 +30,7 @@ public class BoardImage {
     private Long id;
 
     @Column(name = "image_url", nullable = false)
+    @Pattern(regexp = IMAGES_PATTERN, message = IMAGES_PATTERN__REQUIREMENT)
     private String imageUrl;
 
     @Column(name = "image_type", nullable = false)
